399da6f4f6e1dfe0fdce0d22ec16c16a9f9ccc9c
Daniel Pomfret <dan@coly.uk>
2022-01-09 00:21:19 +0000

    New gamertag image

--- src/index.js
+++ src/index.js
@@ -12,10 +12,9 @@ const config = require('./config');
 const app = express();
 const port = config.PORT || 3000;
 
-const buffer = fs.readFileSync(`${__dirname}/html/match.html`);
-
 app.get('/generate/match/:Id', async (req, res) => {
 	try {
+		const buffer = fs.readFileSync(`${__dirname}/html/match.html`);
 		const matchData = await Halo.getMatchData(req.params.Id);
 
 		const image = await nodeHtmlToImage({
@@ -40,6 +39,52 @@ app.get('/generate/match/:Id', async (req, res) => {
 		}
 		else {
 			await S3.uploadFile(image, `match/${req.params.Id}`);
+			res.setHeader('Content-Type', 'application/json');
+			res.end(JSON.stringify({ status: 'generated' }));
+		}
+	}
+	catch (e) {
+		console.log(e);
+		res.setHeader('Content-Type', 'application/json');
+		res.end(JSON.stringify({ status: 'error' }));
+	}
+});
+
+app.get('/generate/gamertag/:gamerTag/:Id', async (req, res) => {
+	try {
+		const buffer = fs.readFileSync(`${__dirname}/html/gamertag.html`);
+		const playerStats = await Halo.getPlayerStats(req.params.gamerTag);
+		const gamerTagAppearance = await Halo.getGamerTagAppearance(req.params.gamerTag);
+
+		const image = await nodeHtmlToImage({
+			html: buffer.toString(),
+			quality: 100,
+			content: { 
+				gamerTag: req.params.gamerTag, 
+				gamerTagAppearance, 
+				stats: playerStats, 
+				accuracy: playerStats.core.shots.accuracy.toFixed(2) + '%',
+				kda: playerStats.core.kda.toFixed(2),
+				kdr: playerStats.core.kdr.toFixed(2),
+			},
+			puppeteerArgs: {
+				headless: true,
+				args: [
+					'--no-sandbox',
+					'--disable-setuid-sandbox',
+					'--headless',
+					'--disable-gpu',
+					'--disable-dev-shm-usage',
+				],
+			},
+		});
+
+		if (config.IS_LOCAL) {
+			res.writeHead(200, { 'Content-Type': 'image/png' });
+			res.end(image, 'binary');
+		}
+		else {
+			await S3.uploadFile(image, `gamertag/${req.params.Id}`);
 			res.setHeader('Content-Type', 'application/json');
 			res.end(JSON.stringify({ status: 'generated' }));
 		}
--- src/lib/halo.js
+++ src/lib/halo.js
@@ -1,7 +1,18 @@
 const config = require('../config');
 const lib = require('lib')({ token: config.STDLIB_SECRET_TOKEN });
-
 const HaloAPI = lib.halo.infinite['@0.3.3'];
+
+const getLastMatch = async (gamertag) => {
+	const result = await HaloAPI.stats.matches.list({
+		gamertag: gamertag,
+		limit: {
+			count: 1,
+			offset: 0,
+		},
+		mode: 'matchmade',
+	});
+	return result.data[0];
+};
 
 const getMatchData = async (matchId) => {
 	const result = await HaloAPI.stats.matches.retrieve({
@@ -11,4 +22,21 @@ const getMatchData = async (matchId) => {
 	return result.data;
 };
 
-module.exports = { getMatchData };
+const getPlayerStats = async (gamertag) => {
+	const result = await HaloAPI.stats['service-record'].multiplayer({
+		gamertag: gamertag,
+		experience: 'pvp-only',
+	});
+
+	return result.data;
+};
+
+const getGamerTagAppearance = async (gamertag) => {
+	const result = await HaloAPI.appearance({
+		gamertag: gamertag,
+	});
+
+	return result.data;
+};
+
+module.exports = { getLastMatch, getMatchData, getPlayerStats, getGamerTagAppearance };
